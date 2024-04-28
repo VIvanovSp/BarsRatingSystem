69bfc986efd4a3a6c8e7e23a1c6f67bbeca64e05
VIvanovSp <132898423+VIvanovSp@users.noreply.github.com>
2024-04-28 05:55:12 -0700

    Add files via upload

---
+++ BarRatingSystem/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BarRatingSystem.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Additional properties as needed
+    }
+}
