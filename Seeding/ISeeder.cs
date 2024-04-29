ff9dc3afdba04924a120bca385337cdd5c8ae7df
VIvanovSp <132898423+VIvanovSp@users.noreply.github.com>
2024-04-29 00:27:19 -0700

    Delete Seeding directory

--- Seeding/ISeeder.cs
+++
@@ -1,9 +0,0 @@
-﻿using Bar_Rating.Data;
-
-namespace Bar_Rating.Seeding
-{
-    public interface ISeeder
-    {
-        Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider);
-    }
-}
